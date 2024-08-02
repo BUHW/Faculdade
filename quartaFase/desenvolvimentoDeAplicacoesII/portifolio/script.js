823a56da9864077ab041c19654083af6c5e1cc12
BUHW <victorapereira444013@gmail.com>
2024-08-02 20:19:58 -0300

    feat: criado card

--- quartaFase/desenvolvimentoDeAplicacoesII/portifolio/script.js
+++ quartaFase/desenvolvimentoDeAplicacoesII/portifolio/script.js
@@ -1,4 +1,85 @@
-var radio = document.getElementById('manual-btn');
+// Generator repositories
+
+var repositories = document.getElementById('repositories')
+
+const cardRepositories = [
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Calculadora',
+        img: './img/calculadora.png',
+        descricao: 'Calculadora em JS',
+        linkRepository: 'https://github.com/BUHW/Calculadora' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+    {
+        name: 'Comafe',
+        img: './img/logoSemFundo.png',
+        descricao: 'Site empresarial',
+        linkRepository: 'https://github.com/BUHW/COMAFE' 
+    },
+]
+
+cardRepositories.forEach((card) => {
+    repositories.innerHTML += `
+        <div class="card">
+            <div class="image-container">
+                <img src="${card.img}" alt="${card.name}" />
+            </div>
+            <div class="card-content">
+                <h2>${card.name}</h2>
+                <h4>${card.descricao}</h4>
+                <a href="${card.linkRepository}" target="_blank"><button>VER REPOSITÓRIO</button></a>
+            </div>
+        </div>    
+    `
+});
+
 var cont = 1
 
 document.getElementById('radio1').checked = true
@@ -15,3 +96,4 @@ function nextCard() {
     }
     document.getElementById('radio' + cont).checked = true
 }
+
