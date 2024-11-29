0d004b397c8489aa70e30fef69f8ff55d5027ed8
bernardoburigo <bernardoburigo@hotmail.com>
2024-11-28 23:11:41 -0300

    fix: organização da pasta utils

---
+++ quartaFase/desenvolvimentoDeAplicacoesII/Trabalho02/backend/api/utils/models/camposComuns.js
@@ -0,0 +1,19 @@
+
+const { DataTypes } = require('sequelize');
+
+const camposComuns = {
+    cancelado: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+};
+
+module.exports = camposComuns;
