41bd7ea4f3bf037416ff4ebbd3384cff311ddf55
bernardoburigo <bernardoburigo@hotmail.com>
2024-11-28 22:51:26 -0300

    fix: organização das pastas

--- quartaFase/desenvolvimentoDeAplicacoesII/Trabalho02/utils/database_postgre.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('banco', 'usuario', 'senha', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
-
-module.exports = sequelize;
