26311c23a4d2e18c82b5ea054a3a297544c06b96
bernardoburigo <bernardoburigo@hotmail.com>
2024-12-01 01:45:14 -0300

    fix: refatoração usecase de login

--- quartaFase/desenvolvimentoDeAplicacoesII/Trabalho02/backend/api/auth/controllers/usecases/loginUseCase.js
+++ quartaFase/desenvolvimentoDeAplicacoesII/Trabalho02/backend/api/auth/controllers/usecases/loginUseCase.js
@@ -1,44 +1,51 @@
 require('dotenv').config();
-const db = require('../../../utils/database/database_postgres')
+const Usuarios = require('../model/usuarios');
+const Permissoes = require('../model/permissoes');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 exports.login = async (req, res, next) => {
+    try {
+        const { login, senha } = req.body;
 
-    const login = req.body.login.toLowerCase();
-    const senha = req.body.senha;
-
-    var user = '';
-    if (login && senha) {
-        user = await db.usuarios.scope('senha').findOne({ where: { login: login, cancelado: false } })
-        try {
-            if (!await bcrypt.compare(senha, user.senha)) {
-                user = ''
-            }
-        } catch {
-            user = '';
+        if (!login || !senha) {
+            return res.status(400).json({ message: 'Login e senha são obrigatórios.' });
         }
 
-    }
-    if (!user) {
-        res.status(400).send('Usuário ou senha inválido');
-    } else {
-        perm = await db.permissoes.findAll({ attributes: ['grupo', 'nome', 'cancelado'], where: { id: user.id } })
-        const token = jwt.sign({
-            i_usuario: user.id,
-            login: user.login,
-        },
+        const user = await Usuarios.scope('senha').findOne({
+            where: { login: login.toLowerCase(), cancelado: false }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Usuário ou senha inválido.' });
+        }
+
+        const senhaValida = await bcrypt.compare(senha, user.senha);
+        if (!senhaValida) {
+            return res.status(400).json({ message: 'Usuário ou senha inválido.' });
+        }
+
+        const permissoes = await Permissoes.findAll({
+            attributes: ['grupo', 'nome', 'cancelado'],
+            where: { id: user.id, cancelado: false }
+        });
+
+        const token = jwt.sign(
+            { i_usuario: user.id, login: user.login },
             process.env.ACCESS_TOKEN,
-            { expiresIn: "24h" }
-        )
-        res.status(200).send({
+            { expiresIn: '24h' }
+        );
+
+        return res.status(200).json({
             message: 'Autorizado',
             i_usuario: user.id,
             nome: user.nome,
             login: user.login,
             token,
-            perm
+            permissoes
         });
+    } catch (error) {
+        console.error('Erro no login:', error);
+        return res.status(500).json({ message: 'Erro interno do servidor.' });
     }
-}
-
+};
